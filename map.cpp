af4f3db3a41e0a89066f9d2826232bdefad38e0b
wtp <15908282659@163.com>
2020-12-07 23:18:52 +0800

    add map

--- map.cpp
+++ map.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 #include <map>
 typedef std::map<std::string, int> maptype;
-void mapTest(void)
-{
-	maptype map1;
-	map1.insert(pair<string, int>("xyhello", 10));
-	map1.insert(pair<string, int>("qdasd", 12));
-	map1.insert(pair<string, int>("vfdss", 34));
-}
-void mapPri(std::map<string, int> &m)
-{
-	
-}
+/*
+1.两数之和
+给定一个整数数组 nums 和一个目标值 target，
+请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
+/*
+*知识点：
+*	
+*
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int> res;
+		multimap<int, int> temp;
+        for(int i=0; i<len; i++)
+        {
+            if(nums[i] != temp.find(nums[i]) && nums[i] <= target)
+			{
+				temp.insert(pair<nums[i], i);
+			}
+			else if(nums[i] == temp.find(nums[i]))
+			{
+				
+			}
+        }
+    }
+};
 
 int main(void)
 {
