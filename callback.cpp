39afc2a06051cc861db233862066222c068d8531
wtp <15908282659@163.com>
2020-12-13 21:10:41 +0800

    perfect callback

--- callback.cpp
+++ callback.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
+using namespace std;
 
-namespace XYDEV {
+class RTSPStream{
+public:
+    class RTSPCallback{
+    public:
+        virtual ~RTSPCallback(){};
+        virtual void priCallback() = 0;
+    };
+public:
+    RTSPStream(RTSPCallback *callback);
+    ~RTSPStream();
+    RTSPCallback *_callback;
+};
+RTSPStream::RTSPStream(RTSPCallback *callback)
+{
+    _callback = callback;
+}
 
 class RTSPCameraManager {
 public:
     RTSPCameraManager();
     ~RTSPCameraManager();
-    start();
-    stop();
-private:
+    RTSPStream* getNance();
     void priCallback(void);
+};
+RTSPStream* RTSPCameraManager::getNance()
+{
+    RTSPStream *newStream = new RTSPStream(this);
+    return newStream;
 }
 void RTSPCameraManager::priCallback(void)
 {
     cout << "Calling priCallback suc!" << endl;
 }
 
-}
+int main(void)
+{
+    RTSPCameraManager  a;
+    RTSPStream *pStream = a.getNance();
+    pStream->_callback->priCallback();
 
-namespace XYDEVC{
-
-class RTSPStream{
-    class RTSPCallback{
-        virtual ~RTSPCallback(){};
-        virtual void priCallback() = 0;
-    }
-public:
-    RTSPStream();
-    ~RTSPStream();
-    start();
-    stop();
-private:
-}
-
+    return 0;
 }
