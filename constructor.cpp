16d0522d0c81eda04e4d01842641cd7ff63d0474
台平 魏 <weitaiping@xylink.com>
2020-12-14 15:42:31 +0800

    prefect move

--- constructor.cpp
+++ constructor.cpp
@@ -9,7 +9,11 @@ using namespace std;
 		所有的具名变量或者对象都是左值，而右值不具名。
 	2、右值引用
 		demo &&n
-	2、移动构造函数
+    2、std::move()
+        将左值引用转换为右值引用
+        避免不必要的拷贝操作
+        将对象的状态或者所有权从一个对象转移到另一个对象，只是转移，没有内存的搬迁或者内存拷贝
+	3、移动构造函数  
 		在创建一个新对象时有一个旧的相同对象需要回收，
 		这时就可以用移动构造函数把就对象直接给新对象
 */
@@ -51,11 +55,9 @@ int main(void)
     cout << b.getNum() << endl;
     
 
-    demo a(getDemo());
-    cout << a.getNum() << endl;
-    demo c1(std::move(a));      //移动构造函数
+    demo c1(std::move(c));      //移动构造函数
     cout << c1.getNum() << endl;
 
-    //cout << "a:" << a.getNum() << endl;
+    //cout << "c:" << c.getNum() << endl;
     return 0;
 }
