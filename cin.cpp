f750da569613461542bdfd9c3b59816c83b559fd
台平 魏 <weitaiping@xylink.com>
2021-01-15 16:41:28 +0800

    char cin int

---
+++ cin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <list>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+int main()
+{
+	int val;
+	cout << "Please input value: ";
+	cin >> val;
+	cout << "val is value: "<< val << endl;
+	return 0;
+}
